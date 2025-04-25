6a886ef219184b5e97e551dc410050d54c5fb922
Jesus Castro <castrojesus39@gmail.com>
2025-04-24 21:11:38 -0700

    used SpectreConsole to create table for the menu

--- DrinksMenuApp/Menu.cs
+++ DrinksMenuApp/Menu.cs
@@ -8,7 +8,7 @@ public class Menu
     {
         // AnsiConsole.Markup("[cyan]Welcome to the Cantina![/]");
 
-        string drink = AnsiConsole.Ask<string>("What drink?: ");
+        /* string drink = AnsiConsole.Ask<string>("What drink?: ");
         DrinksService ds = new DrinksService();
 
         ApiResponse response = await ds.GetDrinksByCategory(drink);
@@ -16,7 +16,33 @@ public class Menu
         foreach(var d in response.Drinks)
         {
             Console.WriteLine($"{d.idDrink} {d.strDrink} {d.strCategory}");
+        } */
+
+        string colName = "Menu";
+
+        string[] choices = {"Ordinary Drinks", "Cocktail", "Milk / Float / Shake", "Other / Unknown", "Cocoa", "Shot", "Coffee / Tea", "Homemade Liqueur", "Punch / Party Drink", "Beer", "Soft Drink / Soda"};
+
+        var table = new Table();
+        table.AddColumns(colName);
+
+        foreach(var choice in choices)
+        {
+            table.AddRow(choice);
         }
+
+        table.Border(TableBorder.Rounded);
+        table.Centered();
+
+        AnsiConsole.Write(table);
+
+        // var option = AnsiConsole.Prompt(
+        //         new SelectionPrompt<string>().Title("What kind of drink are you looking for?").AddChoices(choices)
+        //         );
+        //
+        // Console.WriteLine($"Your option: {option}");
+
+
+
 
     }
 }
