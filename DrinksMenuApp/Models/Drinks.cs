d6f1e3de0cc88d0738c16cb0da60805710591adf
Jesus Castro <castrojesus39@gmail.com>
2025-04-27 18:39:59 -0700

    took into consideration null values

--- DrinksMenuApp/Models/Drinks.cs
+++ DrinksMenuApp/Models/Drinks.cs
@@ -2,12 +2,12 @@ namespace DrinksMenuApp.Models;
 
 public class ApiResponse
 {
-    public List<Drink> Drinks { get; set; }
+    public List<Drink> Drinks { get; set; } = new List<Drink>();
 }
 
 public class Drink {
-    public string idDrink { get; set; }
-    public string strDrink { get; set; }
-    public string strCategory { get; set; }
-    public string strAlcoholic { get; set; }
+    public string idDrink { get; set; } = String.Empty;
+    public string strDrink { get; set; } = String.Empty;
+    public string strCategory { get; set; } = String.Empty;
+    public string strAlcoholic { get; set; } = String.Empty;
 }
