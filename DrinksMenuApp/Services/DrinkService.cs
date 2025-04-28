e6d536a1e991a6962ac4637df616181d0f37121c
Jesus Castro <castrojesus39@gmail.com>
2025-04-27 18:40:43 -0700

    added try catch statement for better erorr handling, and changed variable name to category instead of drink

--- DrinksMenuApp/Services/DrinkService.cs
+++ DrinksMenuApp/Services/DrinkService.cs
@@ -1,5 +1,7 @@
 using System.Text.Json;
 using DrinksMenuApp.Models;
+using Spectre.Console;
+
 namespace DrinksMenuApp.Services;
 
 public class DrinksService : IDrinksService
@@ -11,16 +13,26 @@ public class DrinksService : IDrinksService
         _httpClient = new HttpClient();
     }
 
-    public async Task<ApiResponse> GetDrinksByCategory(string drinkName)
+    public async Task<ApiResponse> GetDrinksByCategory(string category)
     {
-        var response = await _httpClient.GetStringAsync(
-            $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drinkName}"
-        );
+        try
+        {
+            var response = await _httpClient.GetStringAsync(
+                $"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={category}"
+            );
 
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            
+            var data = JsonSerializer.Deserialize<ApiResponse>(response, options) ?? new ApiResponse();
 
-        var data = JsonSerializer.Deserialize<ApiResponse>(response, options);
+            return data;
+
+        }
+        catch (HttpRequestException e)
+        {
+            AnsiConsole.MarkupLine($"[red]Request Error: {e.Message}[/]");
+        }
         
-        return data;
+        return new ApiResponse();
     }
 }
