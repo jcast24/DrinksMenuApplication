d493c5908c662bfbb1db045cad60391b0a4b5b5c
Jesus Castro <castrojesus39@gmail.com>
2025-04-28 18:35:52 -0700

    included other fields that are shown in the api to give better details of drinks

--- DrinksMenuApp/Models/Drinks.cs
+++ DrinksMenuApp/Models/Drinks.cs
@@ -5,9 +5,27 @@ public class DrinkResponse
     public List<Drink> Drinks { get; set; } = new List<Drink>();
 }
 
-public class Drink {
+public class Drink
+{
     public string idDrink { get; set; } = String.Empty;
     public string strDrink { get; set; } = String.Empty;
     public string strCategory { get; set; } = String.Empty;
     public string strAlcoholic { get; set; } = String.Empty;
+    public string strTags { get; set; } = String.Empty;
+    public string strInstructions { get; set; } = String.Empty;
+    public string strIngredients1 { get; set; } = String.Empty;
+    public string strIngredients2 { get; set; } = String.Empty;
+    public string strIngredients3 { get; set; } = String.Empty;
+    public string strIngredients4 { get; set; } = String.Empty;
+    public string strIngredients5 { get; set; } = String.Empty;
+    public string strIngredients6 { get; set; } = String.Empty;
+    public string strIngredients7 { get; set; } = String.Empty;
+    public string strIngredients8 { get; set; } = String.Empty;
+    public string strIngredients9 { get; set; } = String.Empty;
+    public string strIngredients10 { get; set; } = String.Empty;
+    public string strIngredients11 { get; set; } = String.Empty;
+    public string strIngredients12 { get; set; } = String.Empty;
+    public string strIngredients13 { get; set; } = String.Empty;
+    public string strIngredients14 { get; set; } = String.Empty;
+    public string strIngredients15 { get; set; } = String.Empty;
 }
