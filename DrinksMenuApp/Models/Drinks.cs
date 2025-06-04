1ab86e9aae511bcf83180e113b50ae222e051dbd
Jesus Castro <castrojesus39@gmail.com>
2025-06-03 21:40:19 -0700

    updated Drinks model

--- DrinksMenuApp/Models/Drinks.cs
+++ DrinksMenuApp/Models/Drinks.cs
@@ -2,30 +2,30 @@ namespace DrinksMenuApp.Models;
 
 public class DrinkResponse
 {
-    public List<Drink> Drinks { get; set; } = new List<Drink>();
+    public List<Drink>? Drinks { get; set; }
 }
 
 public class Drink
 {
-    public string idDrink { get; set; } = String.Empty;
-    public string strDrink { get; set; } = String.Empty;
-    public string strCategory { get; set; } = String.Empty;
-    public string strAlcoholic { get; set; } = String.Empty;
-    public string strTags { get; set; } = String.Empty;
-    public string strInstructions { get; set; } = String.Empty;
-    public string strIngredients1 { get; set; } = String.Empty;
-    public string strIngredients2 { get; set; } = String.Empty;
-    public string strIngredients3 { get; set; } = String.Empty;
-    public string strIngredients4 { get; set; } = String.Empty;
-    public string strIngredients5 { get; set; } = String.Empty;
-    public string strIngredients6 { get; set; } = String.Empty;
-    public string strIngredients7 { get; set; } = String.Empty;
-    public string strIngredients8 { get; set; } = String.Empty;
-    public string strIngredients9 { get; set; } = String.Empty;
-    public string strIngredients10 { get; set; } = String.Empty;
-    public string strIngredients11 { get; set; } = String.Empty;
-    public string strIngredients12 { get; set; } = String.Empty;
-    public string strIngredients13 { get; set; } = String.Empty;
-    public string strIngredients14 { get; set; } = String.Empty;
-    public string strIngredients15 { get; set; } = String.Empty;
+    public string? idDrink { get; set; }
+    public string? strDrink { get; set; } 
+    public string? strCategory { get; set; } 
+    public string? strAlcoholic { get; set; } 
+    public string? strTags { get; set; } 
+    public string? strInstructions { get; set; } 
+    public string? strIngredients1 { get; set; }
+    public string? strIngredients2 { get; set; }
+    public string? strIngredients3 { get; set; }
+    public string? strIngredients4 { get; set; }
+    public string? strIngredients5 { get; set; }
+    public string? strIngredients6 { get; set; } 
+    public string? strIngredients7 { get; set; } 
+    public string? strIngredients8 { get; set; } 
+    public string? strIngredients9 { get; set; } 
+    public string? strIngredients10 { get; set; }
+    public string? strIngredients11 { get; set; }
+    public string? strIngredients12 { get; set; }
+    public string? strIngredients13 { get; set; }
+    public string? strIngredients14 { get; set; }
+    public string? strIngredients15 { get; set; }
 }
