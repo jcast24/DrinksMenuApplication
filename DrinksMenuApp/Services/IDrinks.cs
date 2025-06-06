7df3af96a17376eda40f82aa70a18f92c646d41f
Jesus Castro <castrojesus39@gmail.com>
2025-06-05 22:03:23 -0700

    re-did IDrinkService file

--- DrinksMenuApp/Services/IDrinks.cs
+++ DrinksMenuApp/Services/IDrinks.cs
@@ -4,9 +4,7 @@ namespace DrinksMenuApp.Services;
 
 public interface IDrinksService
 {
-    Task<List<Drink>> GetDrinksByCategory(string category);
-    Task<List<string>> GetValidCategoriesAsync();
-    Task<List<Drink>> PromptUserForValidCategory();
-    Task ShowDrinks();
-    Task LookupDrinkById();
+    Task<List<string>> GetCategories();
+    Task OutputDataFromGetCategories();
+    Task OutputAllDrinksFromCategoryType();
 }
