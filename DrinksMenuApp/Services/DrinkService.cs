365df92aad40783c154d1de90b3bf7430946267e
Jesus Castro <castrojesus39@gmail.com>
2025-06-10 13:12:09 -0700

    implemented and resolved category data to be used to verify the id for each drink

--- DrinksMenuApp/Services/DrinkService.cs
+++ DrinksMenuApp/Services/DrinkService.cs
@@ -7,10 +7,12 @@ namespace DrinksMenuApp.Services;
 public class DrinkService : IDrinksService
 {
     private readonly HttpClient _httpClient;
+    private string _category;
 
-    public DrinkService(HttpClient httpClient)
+    public DrinkService(HttpClient httpClient, string category)
     {
         _httpClient = httpClient;
+        _category = category;
     }
 
     // Small test to see if the GetCategories method works
@@ -23,6 +25,19 @@ public class DrinkService : IDrinksService
         foreach (var category in categories)
         {
             Console.WriteLine(category);
+        }
+    }
+
+    // Small test to output the drinks based off of the user choosing a specific category
+    public async Task OutputAllDrinksFromCategoryType()
+    {
+        var drinks = await GetAllDrinksByCategory();
+
+        foreach (var drink in drinks)
+        {
+            Console.WriteLine($"ID: {drink.idDrink}");
+            Console.WriteLine($"Drink: {drink.strDrink}");
+            Console.WriteLine("---------------------");
         }
     }
 
@@ -46,19 +61,6 @@ public class DrinkService : IDrinksService
         return categoryResponse?.Drinks?.Select(c => c.StrCategory!).ToList() ?? new List<string>();
     }
 
-    // Small test to output the drinks based off of the user choosing a specific category
-    public async Task OutputAllDrinksFromCategoryType()
-    {
-        var drinks = await GetAllDrinksByCategory();
-
-        foreach (var drink in drinks)
-        {
-            Console.WriteLine($"ID: {drink.idDrink}");
-            Console.WriteLine($"Drink: {drink.strDrink}");
-            Console.WriteLine("---------------------");
-        }
-    }
-
     // Ask user which category of drink they would like to see more about
     public async Task<List<Drink>> GetAllDrinksByCategory()
     {
@@ -79,55 +81,12 @@ public class DrinkService : IDrinksService
         var drinkResponse = JsonSerializer.Deserialize<DrinkResponse>(json, options);
         return drinkResponse?.Drinks ?? new List<Drink>();
     }
-
-    /* public async Task<string> VerifyId()
-    {
-        var drinksList = await GetAllDrinksByCategory();
-        
-        List<string> newList = [];
-
-        foreach(var drink in drinksList)
-        {
-            newList.Add(drink.idDrink!);
-        }
-
-        string chosenId;
-
-        while(true) 
-        {
-            chosenId = AnsiConsole.Ask<string>("Enter the id of a drink: ");
-            if (newList.Contains(chosenId))
-            {
-                break;
-            }
-            AnsiConsole.MarkupLine($"[red]{chosenId} is not a valid id.[/]");
-        }
-        return chosenId;
-    } */
-
-    // Verify the input from the user to see if they enter the right category. If not, ask again.
-    public async Task<string> VerifyCategory()
-    {
-        List<string> validCategories = await GetCategories();
-        string chosenCategory;
-
-        while (true)
-        {
-            chosenCategory = AnsiConsole.Ask<string>("Enter a drink category: ");
-            if (validCategories.Contains(chosenCategory))
-            {
-                break;
-            }
-
-            AnsiConsole.MarkupLine($"[red]{chosenCategory} is not a valid category.[/]");
-        }
-        return chosenCategory;
-    }
-
+    
+    // Show full details of the drink
     public async Task<List<Drink>> GetFullDrinkDetailsById()
     {
-        string id = AnsiConsole.Ask<string>("Enter id of drink: ");
-        // string id = await VerifyId();
+        // string id = AnsiConsole.Ask<string>("Enter id of drink: ");
+        string id = await VerifyId();
         var url = $"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}";
 
         var response = await _httpClient.GetAsync(url);
@@ -156,5 +115,49 @@ public class DrinkService : IDrinksService
             Console.WriteLine($"{drink.strAlcoholic}");
             Console.WriteLine($"{drink.strInstructions}");
         }
+    }
+    
+    // Verify the ID string for the individual drink
+    public async Task<string> VerifyId()
+    {
+        var drinksList = await GetAllDrinksByCategory();
+
+        List<string> newList = [];
+
+        foreach (var drink in drinksList)
+        {
+            newList.Add(drink.idDrink!);
+        }
+
+        string chosenId;
+
+        while (true)
+        {
+            chosenId = AnsiConsole.Ask<string>("Enter the id of a drink: ");
+            if (newList.Contains(chosenId))
+            {
+                break;
+            }
+            AnsiConsole.MarkupLine($"[red]{chosenId} is not a valid id.[/]");
+        }
+        return chosenId;
+    }
+
+    // Verify the input from the user to see if they enter the right category. If not, ask again.
+    public async Task<string> VerifyCategory()
+    {
+        List<string> validCategories = await GetCategories();
+
+        while (true)
+        {
+            // chosenCategory = AnsiConsole.Ask<string>("Enter a drink category: ");
+            if (validCategories.Contains(_category))
+            {
+                break;
+            }
+
+            AnsiConsole.MarkupLine($"[red]{_category} is not a valid category.[/]");
+        }
+        return _category;
     }
 }
