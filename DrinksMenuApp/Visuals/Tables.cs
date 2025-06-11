9c818a5f734f21f77479e35680c4868865bc88e0
Jesus Castro <castrojesus39@gmail.com>
2025-06-10 19:54:18 -0700

    Created a table for the output for all the details to a drink

--- DrinksMenuApp/Visuals/Tables.cs
+++ DrinksMenuApp/Visuals/Tables.cs
@@ -34,4 +34,20 @@ public class Tables
         table.Title("[Cyan]Welcome to the Cantina![/]");
         AnsiConsole.Write(table);
     }
+
+    public static void DrinkDetails(string drinkName, string drinkCategory, string drinkAlcoholic, string drinkInstructions)
+    {
+        var table = new Table();
+
+        table.AddColumn("Full Drink Detail");
+
+        string[] drinkDetails = [drinkName, drinkCategory, drinkAlcoholic, drinkInstructions];
+        foreach(var drink in drinkDetails)
+        {
+            table.AddRow(drink);
+        }
+
+        AnsiConsole.Write(table);
+
+    }
 }
